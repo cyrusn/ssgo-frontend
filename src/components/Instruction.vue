4d405444df28d0e03ef8944f246c6716059550b5
cyrusn <cyrusncy@gmail.com>
2019-02-23 13:40:43 +0800

    pull out instraction as component

---
+++ src/components/Instruction.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert alert-danger">
+    同學必須完成網上{{isMock ? '模擬' : null}}選科。列印選科意向表並交家長簽署，並於{{formatDate(deadline)}}或之前交予班主任。{{isMock ? null: '是次為正式選科，選科回條一經繳交，將不能更改志願。'}}如有疑問，可向{{committeeInCharge}}{{pics[0]}}老師或{{pics[1]}}老師查詢。
+    <ul>
+      <li>將所有選科組合排好優次</li>
+      <li>確定選科意向</li>
+      <li>列印選科意向表</li>
+      <li>家長簽署</li>
+      <li>{{formatDate(deadline)}}或之前交予班主任</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Formatter from '@/chineseDateFormatter'
+import { isMock, committeeInCharge, pics, deadline } from '@/config.js'
+
+export default {
+  data () {
+    return {
+      isMock,
+      deadline,
+      pics,
+      committeeInCharge
+    }
+  },
+  methods: {
+    formatDate (date) {
+      return new Formatter(date)['simple']
+    }
+  }
+}
+</script>
--- src/views/Student/Selection.vue
+++ src/views/Student/Selection.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
+    <instruction v-if="!isConfirmed"/>
     <confirm-priorities/>
-    <div class="card" v-if='!isConfirmed'>
+    <div class="card" v-if="!isConfirmed">
       <h5 class="card-header bg-light">可供選擇的科目組合</h5>
       <dragable-combination
         :list="availableCombinations"
         :movable="!isConfirmed"
         name="available"
         color="btn-info"
-        />
+      />
     </div>
 
     <div class="card my-4">
-      <h5 class="card-header">已選擇的選科次序
+      <h5 class="card-header">
+        已選擇的選科次序
         <small>（請將以上選科項目拖到以下方格內，並按優次將選科意願排序。）</small>
       </h5>
       <dragable-combination
@@ -20,7 +22,7 @@
         :movable="!isConfirmed"
         name="prioritised"
         :color="isConfirmed? 'btn-success' : 'btn-danger' "
-        />
+      />
     </div>
   </div>
 </template>
@@ -28,24 +30,27 @@
 <script>
 import DragableCombination from '@/components/DragableCombination'
 import ConfirmPriorities from '@/components/ConfirmPriorities'
+import Instruction from '@/components/Instruction'
 import combinations from '@/data/combination'
 
 import _ from 'lodash'
-import {mapState} from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   components: {
-    DragableCombination, ConfirmPriorities
+    DragableCombination,
+    ConfirmPriorities,
+    Instruction
   },
   computed: {
     ...mapState('student', ['priorities', 'isConfirmed']),
     availableCombinations () {
-      const {priorities} = this
+      const { priorities } = this
       return _.filter(combinations, comb => !_.includes(priorities, comb.id))
     },
     prioritisedCombinations () {
-      const {priorities} = this
-      return _.map(priorities, id => _.find(combinations, {id}))
+      const { priorities } = this
+      return _.map(priorities, id => _.find(combinations, { id }))
     }
   }
 }
