db79830b93ac679b45a5175b35fc6e9fd8cdd330
cyrusn <cyrusncy@gmail.com>
2019-08-13 16:03:34 +0800

    update information

--- src/components/AllocationResult.vue
+++ src/components/AllocationResult.vue
@@ -16,12 +16,12 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import _ from 'lodash'
-import combinations from '@/data/combination'
-import subjects from '@/data/subject'
-import SubjectOccupiedCounters from '@/components/SubjectOccupiedCounters'
-import DownloadResultsButtonGroup from '@/components/DownloadResultsButtonGroup'
+import { mapState } from "vuex"
+import _ from "lodash"
+import combinations from "@/data/combination"
+import subjects from "@/data/subject"
+import SubjectOccupiedCounters from "@/components/SubjectOccupiedCounters"
+import DownloadResultsButtonGroup from "@/components/DownloadResultsButtonGroup"
 
 const defaultCounters = {
   bio: 0,
@@ -34,7 +34,7 @@ const defaultCounters = {
   cscb: 0,
   econ: 0,
   geog: 0,
-  hsmc: 0,
+  hmsc: 0,
   hist: 0,
   ict: 0
 }
@@ -44,20 +44,20 @@ export default {
     SubjectOccupiedCounters,
     DownloadResultsButtonGroup
   },
-  data () {
+  data() {
     return {
       counters: defaultCounters,
       occupiedSubjects: []
     }
   },
   computed: {
-    ...mapState('students', ['students']),
-    ...mapState('subject', ['capacities']),
-    filterdStudents () {
+    ...mapState("students", ["students"]),
+    ...mapState("subject", ["capacities"]),
+    filterdStudents() {
       const { filter, students } = this
       return filter(students)
     },
-    allocationResults () {
+    allocationResults() {
       const {
         resetCounters,
         allocate,
@@ -71,18 +71,18 @@ export default {
     }
   },
   methods: {
-    findSubject (code) {
+    findSubject(code) {
       return _.find(subjects, { code })
     },
-    resetOccupiedSubjects () {
+    resetOccupiedSubjects() {
       this.occupiedSubjects = []
     },
-    pushOccupiedSubjects (id) {
+    pushOccupiedSubjects(id) {
       const { occupiedSubjects } = this
       if (_.includes(occupiedSubjects, id)) return
       occupiedSubjects.push(id)
     },
-    updateOccupiedSubjects () {
+    updateOccupiedSubjects() {
       const { capacities, pushOccupiedSubjects } = this
       _.forOwn(this.counters, (v, k) => {
         if (v === capacities[k]) {
@@ -90,16 +90,16 @@ export default {
         }
       })
     },
-    resetCounters () {
+    resetCounters() {
       this.counters = Object.assign({}, defaultCounters)
     },
-    updateCounter (code) {
+    updateCounter(code) {
       this.counters[code] += 1
     },
-    getSubjectCodes (id) {
+    getSubjectCodes(id) {
       return _.find(combinations, { id }).subjects
     },
-    isAvailable (id) {
+    isAvailable(id) {
       const { getSubjectCodes, counters, capacities } = this
       const codes = getSubjectCodes(id)
       const subject1 = codes[0]
@@ -109,7 +109,7 @@ export default {
 
       return validSubject1 && validSubject2
     },
-    makeOffers (priorities) {
+    makeOffers(priorities) {
       let offers
       let orders
       let preference
@@ -126,9 +126,9 @@ export default {
           const codes = getSubjectCodes(id)
           codes.forEach(updateCounter)
 
-          offers = _.zipObject(['subject1', 'subject2'], codes)
+          offers = _.zipObject(["subject1", "subject2"], codes)
           orders = _.zipObject(
-            ['subject1', 'subject2'],
+            ["subject1", "subject2"],
             codes.map(code => counters[code])
           )
           preference = i + 1
@@ -143,19 +143,19 @@ export default {
         orders
       }
     },
-    filter (students) {
+    filter(students) {
       return _(students)
         .filter({ isConfirmed: true })
         .filter(s => s.priorities.length === combinations.length)
         .filter(s => s.rank > 0)
-        .orderBy('rank')
+        .orderBy("rank")
         .value()
     },
-    allocate (students) {
+    allocate(students) {
       const { makeOffers } = this
       return _(students)
         .map(s => Object.assign({}, s, makeOffers(s.priorities)))
-        .orderBy(['classcode', 'classno'])
+        .orderBy(["classcode", "classno"])
         .value()
     }
   }
--- src/config.js
+++ src/config.js
@@ -6,13 +6,13 @@ const schoolWebsite = 'https://liping.edu.hk'
 const committeeInCharge = '生涯規劃部'
 const committeeWebsite = 'https://careers.liping.edu.hk'
 const schoolYear = '2018-19'
-const noticeCode = '28/2018-2019'
+const noticeCode = '38/2018-2019'
 const pics = ['曾偉俊', '葉文駿']
 const committeeHead = '顏昭洋'
-const deliveryDate = '2019-04-03'
+const deliveryDate = '2019-06-20'
 const returnYear = '2019'
-const deadline = '2019-04-10'
-const isMock = true
+const deadline = '2019-06-26'
+const isMock = false
 const serverBase = 'https://careers.liping.edu.hk/ss'
 
 export {
